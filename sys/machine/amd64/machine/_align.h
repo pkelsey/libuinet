a89580320ddf4a8b1d745dff54cd9b8470d37b97
Patrick Kelsey <kelsey@ieee.org>
2013-04-16 19:51:53 +0000

    Removed synthetic machine-specific directory structure and modified Makefile to use the canonical kernel tree structure by making a copy of machine-specific includes at build time that can be used in the build.

--- sys/machine/amd64/machine/_align.h
+++
@@ -1,6 +0,0 @@
-/*-
- * This file is in the public domain.
- */
-/* $FreeBSD: release/9.1.0/sys/amd64/include/_align.h 215856 2010-11-26 10:59:20Z tijl $ */
-
-#include <x86/_align.h>
--- sys/machine/arm/machine/sigframe.h
+++
@@ -1,2 +0,0 @@
-/* $FreeBSD: release/9.1.0/sys/arm/include/sigframe.h 129198 2004-05-14 11:46:45Z cognet $ */
-#include <machine/frame.h>
