5546b5429820998dcb7ec595dc2067fa74c80d21
Patrick Kelsey <kelsey@ieee.org>
2014-01-16 03:31:08 +0000

    Added network/uinet/lib.mk so wanproxy programs that want to use libuinet can do so by adding network/uinet to USE_LIBS.

---
+++ lib.mk
@@ -0,0 +1,10 @@
+CPPFLAGS+=-I${TOPDIR}/network/uinet/lib/libuinet/api_include
+CPPFLAGS+=-DHAVE_UINET=1
+
+LDADD+=         -L${TOPDIR}/network/uinet/lib/libuinet
+LDADD+=		-luinet
+ifeq "${OSNAME}" "Linux"
+LDADD+=		-lcrypto
+else
+LDADD+=		-lssl
+endif
