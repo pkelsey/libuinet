4e58c6ee1741c85443d13fb008bbcc1605685a79
Patrick Kelsey <kelsey@ieee.org>
2014-04-09 22:36:05 -0400

    Oops, last patch was misplaced.  Now placed properly.

--- sys/netinet/tcp_syncache.c
+++ sys/netinet/tcp_syncache.c
@@ -1021,7 +1021,7 @@ syncache_passive_client_socket(struct syncache *sc, struct socket *lso, struct m
 	struct mbuf *m1;
 	struct tcphdr *th;
 
-	so = sonewconn_passive_client(lso, 0);
+	so = sonewconn_passive_client(lso, SS_ISCONNECTING);
 	if (so == NULL) {
 		/*
 		 * Drop the connection; we will either send a RST or
@@ -1299,7 +1299,7 @@ syncache_socket(struct syncache *sc, struct socket *lso, struct mbuf *m)
 	 * connection when the SYN arrived.  If we can't create
 	 * the connection, abort it.
 	 */
-	so = sonewconn(lso, SS_ISCONNECTED);
+	so = sonewconn(lso, 0);
 	if (so == NULL) {
 		/*
 		 * Drop the connection; we will either send a RST or
