cb97dc07345dcc605e2d2ca390deaa77c2df9df3
Adrian Chadd <adrian@freebsd.org>
2014-04-16 22:04:58 +0000

    Migrate uinet_sysctl() to uinet_sysctlbyname() - it's the correct name for it.
    Implement uinet_sysctl().

--- lib/libuinet/api_include/uinet_config.h
+++ lib/libuinet/api_include/uinet_config.h
@@ -156,8 +156,11 @@ int uinet_config_blackhole(uinet_blackhole_t action);
  * XXX doesn't belong here!
  */
 int
-uinet_sysctl(char *name, char *oldp, size_t *oldplen,
+uinet_sysctlbyname(char *name, char *oldp, size_t *oldplen,
     char *newp, size_t newplen, size_t *retval, int flags);
+int
+uinet_sysctl(int *name, u_int namelen, void *oid, size_t *oldlenp,
+    void *new, size_t newlen, size_t *retval, int flags);
 
 #ifdef __cplusplus
 }
