pkelsey/libuinet

commit b8914c86e1902c62dc39a617c0307f6161655727

Fixed a bug in ev_uinet_stop().  If ev_uinet_stop() was called on a watcher that was being processed in the current event loop iteration, and that watcher was the last remaining watcher for the corresponding socket context, that socket context was not being removed from the previous-pending list.
This could result in one or more of: upcalls being re-enabled for the
corresponding socket, access to and/or corruption of freed memory,
corruption of the pending list.

Also made a cosmetic change of a call to free() to ev_free().

lib/libev/ev.c
@@ -4840,7 +4840,7 @@ ev_uinet_attach (struct uinet_socket *so)
       sokey = uinet_soallocuserctx (so);
       if (-1 == sokey)
 	{
-	  free (ctx);
+	  ev_free (ctx);
 	  ctx = NULL;
 	}
       else
@@ -4857,6 +4857,8 @@ ev_uinet_attach (struct uinet_socket *so)
 void
 ev_uinet_detach (struct ev_uinet_ctx *ctx)
 {
+  assert (("libev: detaching uinet ctx that is still in use", ctx->head == NULL));
+
   ev_free (ctx);
 }
 
@@ -4971,11 +4973,21 @@ ev_uinet_stop (EV_P_ ev_uinet *w) EV_THROW
    * accessing soctx->pend_flags because it will only be checked when there
    * are no watchers and thus no upcalls installed.
    */
-  if ((NULL == soctx->head) && (soctx->pend_flags & EV_UINET_PENDING)) {
-    soctx->pend_flags = EV_NONE;
-    pthread_mutex_lock (&uinet_pend_lock);
-    UINET_LIST_REMOVE (soctx, pend_list);
-    pthread_mutex_unlock (&uinet_pend_lock);
+  if (NULL == soctx->head) {
+	  if (soctx->pend_flags & EV_UINET_INHIBITED) {
+		  /* No lock necessary for the prev_pend list, as it is only
+		   * modified in-loop.
+		   */
+		  UINET_LIST_REMOVE (soctx, pend_list);
+	  } else {
+		  assert (("libev: uinet context neither pending nor inhibited in ev_uinet_stop",
+			   soctx->pend_flags & EV_UINET_PENDING));
+
+		  pthread_mutex_lock (&uinet_pend_lock);
+		  UINET_LIST_REMOVE (soctx, pend_list);
+		  pthread_mutex_unlock (&uinet_pend_lock);
+	  } 
+	  soctx->pend_flags = EV_NONE;
   }
 
 #if EV_WALK_ENABLE

commit 5652342f44575cf7bca31eaac16858da2732928a

The assert() introduced in b8914c8 was too strong in that it forgot completely that a watcher may be stopped by another watcher during an event loop iteration where that watcher had no events of its own.

lib/libev/ev.c
@@ -4979,14 +4979,14 @@ ev_uinet_stop (EV_P_ ev_uinet *w) EV_THROW
 		   * modified in-loop.
 		   */
 		  UINET_LIST_REMOVE (soctx, pend_list);
-	  } else {
-		  assert (("libev: uinet context neither pending nor inhibited in ev_uinet_stop",
-			   soctx->pend_flags & EV_UINET_PENDING));
-
+	  } else if (soctx->pend_flags & EV_UINET_PENDING) {
 		  pthread_mutex_lock (&uinet_pend_lock);
 		  UINET_LIST_REMOVE (soctx, pend_list);
 		  pthread_mutex_unlock (&uinet_pend_lock);
-	  } 
+	  } /* else 
+	     * This watcher is being stopped by some other watcher during an
+	     * event loop iteration where this watcher had no events.
+	     */
 	  soctx->pend_flags = EV_NONE;
   }
 